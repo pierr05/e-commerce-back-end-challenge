d34649318ae85678a7d2887130a7f98f8c36db2c
pierr05 <pierreachlet322@gmail.com>
2023-03-17 21:47:22 -0400

    Added code category model

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,17 @@ class Category extends Model {}
 
 Category.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
     // define columns
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
   },
   {
     sequelize,
